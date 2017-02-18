06d5fb22b19682e2be17ff80c0cffd30451558c3
Vesa Laakso <laakso.vesa@gmail.com>
2017-02-18 17:08:25 +0200

    Muuta Satunnaisuus palikka luokaksi

--- src/visualisointi/Satunnaisuus.js
+++ src/visualisointi/Satunnaisuus.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Satunnaisuus() {
-  return <div>Hei, maailma!</div>
+export default class Satunnaisuus extends Component {
+  render() {
+    return <div>Hei, maailma!</div>;
+  }
 };
